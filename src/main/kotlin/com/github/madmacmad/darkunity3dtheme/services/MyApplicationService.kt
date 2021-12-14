76f186d52067c6bdce2591f151d6934aea17545b
GitHub Action <action@github.com>
2021-12-14 11:02:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/madmacmad/darkunity3dtheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.madmacmad.darkunity3dtheme.services
+
+import com.github.madmacmad.darkunity3dtheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/madmacmad/darkunity3dtheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/madmacmad/darkunity3dtheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.madmacmad.darkunity3dtheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.madmacmad.darkunity3dtheme.MyBundle
 
 class MyProjectService(project: Project) {
 
